12b73845add3be3177caf5b8a0fc594dcc6df30a
ddd <rozinanime@gmail.com>
2026-04-26 21:24:40 +1000

    Update main.py

--- File.read/main.py
+++ File.read/main.py
@@ -1,18 +1,18 @@
 def get_books(filename):
     books = []
     with open(filename, "r", encoding="utf-8") as f:
-        for i, line in enumerate(f):
+        f.readline()
+        for line in f:
             parts = line.strip().split("|")
-            if i > 0:
-                parts[3] = int(parts[3])
-                parts[4] = float(parts[4])
+            parts[3] = int(parts[3])
+            parts[4] = float(parts[4])
             books.append(parts)
     print(books)
     return books
 
 def choice_books(books, keyword):
-    choices = [books[0]]
-    for book in books[1:]:
+    choices = []
+    for book in books:
         if keyword.lower() in book[1].lower():
             choices.append(book)
 
@@ -21,7 +21,7 @@ def choice_books(books, keyword):
 
 def count_books(books):
     count = []
-    for book in books[1:]:
+    for book in books:
         isbn = book[0]
         total_value = round(book[3] * book[4], 2)
         count.append((isbn, total_value))
@@ -29,6 +29,5 @@ def count_books(books):
     return count
 if __name__ == "__main__":
     all_books = get_books("books.csv")
-    count_Bool = choice_books(all_books, "Python")
+    count_Bool = choice_books(all_books, input("Язык программирование: "))
     total = count_books(all_books)
-
